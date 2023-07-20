30240868846e6de3924a9435adf8de15e93b9071
wallodya <78960605+wallodya@users.noreply.github.com>
2023-07-20 18:03:29 +0300

    init

---
+++ create-data.ts
@@ -0,0 +1,20 @@
+
+const MOCK_DATA_PATH = "./data/input.txt"
+
+/** 
+ * Creates file (about 1 GB in size)
+ * with strings separated by space,
+ * 
+*/
+const createData = () => {
+    console.log(">>> Creating test data...")
+    console.time("create-data")
+
+    //TODO implement create-data
+
+    console.timeEnd("create-data")
+    console.log(">>> Data created.")
+    return
+}
+
+createData()
---
+++ main.ts
@@ -0,0 +1,4 @@
+export const addNumbers = (a: number, b : number): number => {
+    return a + b
+}
+console.log("Sum: ", addNumbers(1, 2))
