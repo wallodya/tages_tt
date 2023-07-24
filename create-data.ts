d5d248d26665c60c96b285739b39777a6b5dfe0f
wallodya <78960605+wallodya@users.noreply.github.com>
2023-07-24 16:19:42 +0300

    upd:data dreator class

--- create-data.ts
+++ create-data.ts
@@ -1,72 +1,104 @@
 import fs from "fs"
 import checkDirectory from "./utils/check-dir"
-import RandomStringGenerator from "./utils/random"
+import RandomStringGenerator, { RandomStringGeneratorOptions } from "./utils/random"
 import { INPUT_PATH, MOCK_DATA_AMOUNT, SEPARATOR_IN } from "./constants"
 
-type CreateDataArgs = {
-    path: string,
-    amount: number,
+type MockDataCreatorOptions = Partial<{
+    outputPath: string,
+    minLength: number,
+    maxLength: number,
     separator: string,
-    generator: RandomStringGenerator
-}
+    predefinedStrings: string[]
+}> & RandomStringGeneratorOptions
 
-/** 
- * Creates file (about 1 GB in size)
- * with strings separated by space,
- * 
-*/
-const createData = async ({
-	path,
-	amount,
-    separator,
-    generator
-}: CreateDataArgs) => {
-	console.log(`>>> Creating test data at "${path}"...`)
-	console.time("||| Time: ")
+class MockDataCreator {
+	stringGenerator: RandomStringGenerator
+	private writeHandle: fs.WriteStream
+	stringsAmount: number
+	stringsGenerated: number
+	separator: string
+	outputPath: string
 
-	await checkDirectory(path)
+	constructor({
+		outputPath,
+        separator,
+		minLength,
+		maxLength,
+        predefinedStrings,
+		...generatorOptions
+	}: MockDataCreatorOptions) {
+		this.outputPath = outputPath ?? INPUT_PATH
+        this.separator = separator ?? SEPARATOR_IN
 
-	const stream = fs.createWriteStream(path)
+		this.stringGenerator = new RandomStringGenerator(
+			minLength ?? 1,
+			maxLength ?? 10,
+			{ ...generatorOptions, separator }
+		)
+        this.stringGenerator.predefinedStrings = predefinedStrings ?? []
 
-    let i = 0
+		this.stringsGenerated = 0
+	}
 
-    const writeStrings = () => {
-        while (i < amount) {
-            const randomString = generator.getRandomString()
-            const buffer = Buffer.from(randomString + separator, "utf-8")
-            if (!stream.write(buffer)) {
-                break
-            }
-            ++i
+	async createFile(amount: number) {
+		console.log(`>>> Creating test data at "${this.outputPath}"...`)
+		console.time("||| Time: ")
+
+		await checkDirectory(this.outputPath)
+
+		this.stringsAmount = amount
+		this.writeHandle = fs.createWriteStream(this.outputPath)
+
+		this.writeStrings()
+
+		this.writeHandle.on("drain", () => {
+			this.writeStrings()
+		})
+	}
+
+	createArray(amount: number) {
+        if (amount <= 0) {
+            throw new Error(`
+                Invalid "amount" argument for createArray: ${amount}
+                Mock data amount should be at least 1.  
+            `)
         }
-        if (i >= amount) {
-            stream.close(() => {
-                console.log(">>> Data created.")
-                console.timeEnd("||| Time: ")
-            })
+        if (amount > 1e4) {            
+            throw new Error(`
+                Invalid "amount" argument for createArray: ${amount}
+                Amount is too big. Make sure to use amount below 10_000 or use "createFile" method instead
+            `)
         }
+        return Array(amount).fill(null).map(_ => this.stringGenerator.getRandomString())
     }
 
-    writeStrings()
+	private writeStrings() {
+		while (this.stringsGenerated < this.stringsAmount) {
+			const randomString = this.stringGenerator.getRandomString()
+			const buffer = Buffer.from(randomString + this.separator, "utf-8")
 
-    stream.on("drain", () => {
-        writeStrings()
-    })
+			if (!this.writeHandle.write(buffer)) {
+				break
+			}
+
+			++this.stringsGenerated
+		}
+		if (this.stringsGenerated >= this.stringsAmount) {
+			this.writeHandle.close(() => {
+				console.log(">>> Data created.")
+				console.timeEnd("||| Time: ")
+			})
+		}
+	}
 }
 
-const generator = new RandomStringGenerator(4, 10, {
-    withSpecialSigns: false,
-    withDigits: true,
+const dataCreator = new MockDataCreator({
+    minLength: 4,
+    maxLength: 5,
+    withLetters: false,
+    separator: ","
 })
 
-generator.predefinedStrings = Array(100).fill(null).map(
-    (_) => (
-        String(Math.round(Math.random() * 100))
-    ))
+dataCreator.createFile(MOCK_DATA_AMOUNT)
 
-createData({
-	path: INPUT_PATH,
-	amount: MOCK_DATA_AMOUNT,
-	separator: SEPARATOR_IN,
-    generator
-})
+export default MockDataCreator
--- utils/random.ts
+++ utils/random.ts
@@ -1,93 +1,122 @@
+import { SEPARATOR_IN } from "../constants"
+
+export type RandomStringGeneratorOptions = Partial<{
+	withLetters: boolean
+	withDigits: boolean
+	withSpecialSigns: boolean
+    separator: string
+}>
+
 class RandomStringGenerator {
-    private alphabet: string
-    private digits: string
-    private specialSigns: string
-    private charCollection: string[]
-    private predefined: string[]
+	private alphabet: string
+	private digits: string
+	private specialSigns: string
+	private charCollection: string[]
+	private predefined: string[]
 
 	constructor(
-        private minLength: number,
-        private maxLength: number,
-        private options?: Partial<{
-            withLetters: boolean,
-            withDigits: boolean,
-            withSpecialSigns: boolean,
-        }>
-    ) {
+		public minLength: number,
+		public maxLength: number,
+		private options?: RandomStringGeneratorOptions
+	) {
 		this.minLength = minLength
 		this.maxLength = maxLength
 
-        this.options = {
-            withDigits: options?.withDigits ?? true,
-            withLetters: options?.withLetters ?? true,
-            withSpecialSigns: options?.withSpecialSigns ?? true
-        }
+		this.options = {
+			withDigits: options?.withDigits ?? true,
+			withLetters: options?.withLetters ?? true,
+			withSpecialSigns: options?.withSpecialSigns ?? true,
+            separator: options?.separator ?? SEPARATOR_IN
+		}
 
-        this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        this.digits = "0123456789"
-        this.specialSigns = ".,/()[]{}@$#&*"
+		this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+		this.digits = "0123456789"
+		this.specialSigns = ".,;:/()[]{}@$#&*<>\"'%`~"
 
-        this.charCollection = (
+		this.charCollection = (
 			(this.options.withLetters ? this.alphabet : "") +
 			(this.options.withDigits ? this.digits : "") +
 			(this.options.withSpecialSigns ? this.specialSigns : "")
-		).split("")
+		)
+			.split("")
+			.filter(char => char !== this.options.separator)
 
-        this.predefined = null
+		this.predefined = null
 	}
 
 	getRandomString() {
-        if (this.predefined && this.predefined.length > 0) {
-            return this.getPredefinedString()
-        }
+		if (this.predefined && this.predefined.length > 0) {
+			return this.getPredefinedString()
+		}
 
-        const randomLength = this.getRandomLength()
+		const randomLength = this.getRandomLength()
 
-        let result = ""
-        for (let i = 0; i < randomLength; ++i) {
-            const randomCharIndex = this.getRandomIndex(this.charCollection.length)
-            result += this.charCollection[randomCharIndex]
-        }
+		let result = ""
+		for (let i = 0; i < randomLength; ++i) {
+			const randomCharIndex = this.getRandomIndex(
+				this.charCollection.length
+			)
+			result += this.charCollection[randomCharIndex]
+		}
 
 		return result
 	}
 
 	getRandomNumericString() {
-        const randomLength = this.getRandomLength()
+		const randomLength = this.getRandomLength()
 
-        let result = ""
-        for (let i = 0; i < randomLength; ++i) {
-            const randomDigitIndex = this.getRandomIndex(this.digits.length)
-            result += this.digits[randomDigitIndex]
-        }
+		let result = ""
+		for (let i = 0; i < randomLength; ++i) {
+			const randomDigitIndex = this.getRandomIndex(this.digits.length)
+			result += this.digits[randomDigitIndex]
+		}
 
 		return result
 	}
 
-    set predefinedStrings(arr: string[]) {
-        this.predefined = [...arr]
+	set predefinedStrings(arr: string[]) {
+		this.predefined = [...arr]
+		return
+	}
+
+    set generatorOptions(options: RandomStringGeneratorOptions) {
+        this.options = {
+			withDigits: options?.withDigits ?? this.options.withDigits,
+			withLetters: options?.withLetters ?? this.options.withLetters,
+			withSpecialSigns: options?.withSpecialSigns ?? this.options.withSpecialSigns,
+            separator: options?.separator ?? this.options.separator
+		}
+        
+        this.charCollection = (
+			(this.options.withLetters ? this.alphabet : "") +
+			(this.options.withDigits ? this.digits : "") +
+			(this.options.withSpecialSigns ? this.specialSigns : "")
+		)
+			.split("")
+			.filter(char => char !== this.options.separator)
+
         return
     }
 
-    private getPredefinedString() {
-        if (!this.predefined) {
-            throw new Error("No strings were predefined")
-        }
+	private getPredefinedString() {
+		if (!this.predefined) {
+			throw new Error("No strings were predefined")
+		}
 
-        const randomIndex = this.getRandomIndex(this.predefined.length)
+		const randomIndex = this.getRandomIndex(this.predefined.length)
 
-        return this.predefined[randomIndex]
-    }
+		return this.predefined[randomIndex]
+	}
 
-    private getRandomIndex(length: number) {
-        return Math.round(Math.random() * (length - 1))
-    }
+	private getRandomIndex(length: number) {
+		return Math.round(Math.random() * (length - 1))
+	}
 
-    private getRandomLength() {
-        return Math.round(
+	private getRandomLength() {
+		return Math.round(
 			this.minLength + Math.random() * (this.maxLength - this.minLength)
 		)
-    }
+	}
 }
 
 export default RandomStringGenerator
